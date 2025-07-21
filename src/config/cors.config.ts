b9726b51a201640707bf2a0f0f4997b0caffe01a
Anthonyy Choque <giananthoych@outlook.es>
2025-07-21 08:29:26 -0500

    fix upload max size json

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,8 +1,8 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsConfig: CorsOptions = {
-  //origin: ['http://localhost:5173','https://yw-landing.vercel.app'],
-  origin: '*',
+  origin: ['http://localhost:5173','http://localhost:5174','https://yw-landing.vercel.app','https://yw-front-intranet-oljh-omega.vercel.app'],
+  //origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 };
