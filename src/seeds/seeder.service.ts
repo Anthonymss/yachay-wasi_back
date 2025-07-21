f59736d781ba7b24fba089e9efb2b3c336f6b549
Gian Anthony Choque <140866954+Anthonymss@users.noreply.github.com>
2025-07-21 16:50:59 -0500

    Update seeder.service.ts

--- src/seeds/seeder.service.ts
+++ src/seeds/seeder.service.ts
@@ -22,7 +22,7 @@ export class SeederService {
     await this.areaSeeder.seed();
     await this.adminSeeder.seed();
 
-    await this.volunteerSeeder.seedDynamic(0, 0);
+    await this.volunteerSeeder.seedDynamic(50, 50);
     await this.comunicationPreferenceSeeder.seed();
     await this.beneficiarySeeder.seedDynamic(0);
   }
